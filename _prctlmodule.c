14914c1b894a267f0916334cb044884d38679453
bugchecker <bugchecker@users.noreply.github.com>
2014-12-02 22:14:01 +0500

    Bugfix: prctl_set_proctitle buffer overflow

--- _prctlmodule.c
+++ _prctlmodule.c
@@ -385,7 +385,7 @@ prctl_set_proctitle(PyObject *self, PyObject *args)
     /* Determine up to where we can write */
     len = (int)(argv[argc-1]) + strlen(argv[argc-1]) - (int)(argv[0]);
     strncpy(argv[0], title, len);
-    memset(argv[0] + strlen(title), 0, len);
+    memset(argv[0] + strlen(title), 0, len - strlen(title));
     Py_RETURN_NONE;
 }
 
