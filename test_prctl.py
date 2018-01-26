57cd0a7cad76e8f8792eea22ee5b5d17bae0a90f
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2018-01-26 21:56:35 +0100

    Version 1.7

--- test_prctl.py
+++ test_prctl.py
@@ -1,5 +1,5 @@
 # python-pctrl -- python interface to the prctl function
-# (c)2010-2015 Dennis Kaarsemaker <dennis@kaarsemaker.net>
+# (c)2010-2018 Dennis Kaarsemaker <dennis@kaarsemaker.net>
 # See COPYING for licensing details
 
 import distutils.util
